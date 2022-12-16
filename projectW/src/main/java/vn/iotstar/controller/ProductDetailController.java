cf7d21f5589a9dc3a56256708785658e8156af24
mytran324 <92320923+mytran324@users.noreply.github.com>
2022-12-16 20:17:22 +0700

    review nè hehe

--- projectW/src/main/java/vn/iotstar/controller/ProductDetailController.java
+++ projectW/src/main/java/vn/iotstar/controller/ProductDetailController.java
@@ -9,11 +9,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import vn.iotstar.DAO.CategoryDAO;
 import vn.iotstar.DAO.ProductDAO;
+import vn.iotstar.DAO.ReviewDAO;
 import vn.iotstar.model.Category;
 import vn.iotstar.model.Product;
+import vn.iotstar.model.Review;
 
 @WebServlet(urlPatterns = "/productDetail")
 public class ProductDetailController extends HttpServlet {
@@ -34,10 +37,13 @@ public class ProductDetailController extends HttpServlet {
 		// B1:khỏi tạo DAO
 		ProductDAO productDao = new ProductDAO();
 		CategoryDAO categoryDao = new CategoryDAO();
+		ReviewDAO reviewDao = new ReviewDAO();
 		// B2:sử dụng đối tượng list để chứa danh sách từ ProductDAO
 		Product Product = productDao.getProByIdPro(pid);
 		List<Category> listC = categoryDao.getAll();
+		List<Review> listReview = reviewDao.getAllReviewbyPro(pid);
 		// B3:thiết lập dữ liệu trên jsp
+		req.setAttribute("listReview", listReview);
 		req.setAttribute("getPro", Product);
 		req.setAttribute("listcate", listC);
 		RequestDispatcher rq = req.getRequestDispatcher("views/user/productDetail.jsp");
@@ -46,8 +52,23 @@ public class ProductDetailController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		// thiết lập tiếng việt
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
+		// lay tham so tu Session
+		HttpSession session = req.getSession();
+		Object obj = session.getAttribute("uId");
+		String uid = String.valueOf(obj);
+		String comment = req.getParameter("comment");
+		String rating = req.getParameter("rating");
+		String pid = req.getParameter("pid");
+		// B1:khỏi tạo DAO
+		ReviewDAO reviewDao = new ReviewDAO();
+		// B2:sử dụng đối tượng list để chứa danh sách từ ProductDAO
+		reviewDao.insert(uid, pid, comment, rating);
+		req.setAttribute("pid", pid);
+		doGet(req, resp);
 	}
 
 }
